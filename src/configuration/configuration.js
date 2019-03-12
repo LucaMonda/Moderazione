f992fc48256922ebe4b07d1af404d7a4bdc30168
LucaMonda <luca-mondaini@hotmail.it>
2019-03-12 12:40:47 +0100

    Created new configuration for items

---
+++ src/configuration/configuration.js
@@ -0,0 +1,30 @@
+export const Items = [
+    {
+        value: 0,
+        title: 'OSCENO',
+        desc: 'Offese, insulti, attacchi personali, ecc.',
+        imageGrey: "../images/osceno/grey.svg",
+        checked: false
+    },
+    {
+        value: 1,
+        title: 'MINACCIA',
+        desc: 'Violenza, minacce, provocazioni, ecc.',
+        imageGrey: "../images/minaccia/grey.svg",
+        checked: false
+    },
+    {
+        value: 2,
+        title: 'INSULTO',
+        desc: 'Parolacce, riferimenti al sesso, ecc.',
+        imageGrey: "../images/insulto/grey.svg",
+        checked: false
+    },
+    {
+        value: 3,
+        title: 'RAZZIALE',
+        desc: 'Riferimenti ad etnie, luoghi comuni regionali, ecc.',
+        imageGrey: "../images/razziale/grey.svg",
+        checked: false
+    }
+];

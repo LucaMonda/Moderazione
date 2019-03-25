4a4cc83daee41ad478261758c29c8d4c7f391a08
LucaMonda <luca-mondaini@hotmail.it>
2019-03-25 14:46:18 +0100

    Added dynamic path

--- php/src/Repository/SentenceRepository.php
+++ php/src/Repository/SentenceRepository.php
@@ -2,25 +2,33 @@
 
 namespace App\Repository;
 
-class ModeratorRepository
+class SentenceRepository
 {
+
+    private $path;
+
+    public function __construct($path)
+    {
+        $this->path = $path;
+    }
+
     public function saveInfo($id, $moderator, $categories){
         $obj = (object) array('moderator' => $moderator, 'categories' => $categories);
         //TODO Problem UTF in file_get_contents.
-        $fileJson = json_decode(file_get_contents('../public/sentences.json'),true);
+        $fileJson = json_decode(file_get_contents($this->path),true);
         for ($i=0, $iMax = count($fileJson['sentences']); $i< $iMax; $i++) {
             if ($fileJson['sentences'][$i]['id'] == $id ) {
                 $fileJson['sentences'][$i]['votes'][] = $obj;
             }
         }
         $newJsonString = json_encode($fileJson, JSON_PRETTY_PRINT);
-        file_put_contents('../public/sentences.json', $newJsonString);
+        file_put_contents($this->path, $newJsonString);
         return $newJsonString;
     }
 
     public function getNextSentence(){
         $email = 'stringa-fissa@da-cambiare.it';
-        $fileJson = json_decode(file_get_contents('../public/sentences.json'),true);
+        $fileJson = json_decode(file_get_contents($this->path),true);
         $foundSentence = [];
         foreach($fileJson['sentences'] as $sentence){
             $finded = false;

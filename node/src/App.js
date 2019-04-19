5cb11cd6b79cc88a1afec7d1d35e6db52c564540
LucaMonda <luca-mondaini@hotmail.it>
2019-04-19 10:51:34 +0200

    Refactored App.js

--- node/src/App.js
+++ node/src/App.js
@@ -1,37 +1,38 @@
 import React, {Component} from 'react';
 import AbuseArea from "./components/AbuseArea.jsx"
 import './styles/App.css';
+import Sentence from "./model/sentence";
 
 class App extends Component {
 
     constructor(props){
         super(props);
         this.state = {
-            sentence:{
-                id: null,
-                author: "",
-                content: "",
-                indicators: "",
-            },
+            sentence : new Sentence(),
+            id : null,
+            author : "",
+            content : "",
+            indicators : "",
             disable: false,
         };
-        this.getSentence = this.getSentence.bind(this);
-        this.sendSentence = this.sendSentence.bind(this);
-        this.sentenceTransition = this.sentenceTransition.bind(this);
+
+        this.updateStateAfterGetSentence = this.updateStateAfterGetSentence.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.toggleDisableButton = this.toggleDisableButton.bind(this);
     }
 
     componentDidMount() {
-        this.getSentence();
+        this.state.sentence.getSentence(this.updateStateAfterGetSentence);
     }
 
-    sentenceTransition(color){
-        let sentence = document.getElementById('text');
-        sentence.style.backgroundColor = color;
-        setTimeout(function() {
-            sentence.style.backgroundColor = 'white';
-        }, 600);
+    updateStateAfterGetSentence(disable){
+        this.setState({
+            id : this.state.sentence.getId(),
+            author : this.state.sentence.getAuthor(),
+            content : this.state.sentence.getContent(),
+            indicators : this.state.sentence.getIndicators(),
+            disable: disable
+        });
     }
 
     toggleDisableButton(){
@@ -40,67 +41,9 @@ class App extends Component {
         });
     }
 
-     getSentence(){
-        return fetch('http://localhost:8080/index.php/sentence',{
-            mode: 'cors',
-            headers : {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        })
-        .then(res => res.json())
-        .then(data => {
-            if (data.length !== 0) {
-                this.setState({
-                    id: data.id,
-                    author: data.author,
-                    content: data.content,
-                    indicators : data.indicators,
-                    disable:false
-                });
-                this.sentenceTransition("rgba(135,206,235, 0.3)");
-            }else{
-                this.setState({
-                    id: "",
-                    author: "",
-                    content: "ATTENZIONE! Non ci sono più frasi da moderare.",
-                    indicators : [],
-                    disable:true
-                });
-                this.sentenceTransition("rgba(255,0,0, 0.3)");
-            }
-        })
-    }
-
     async handleSubmit(clickedItems) {
-        await this.sendSentence(clickedItems);
-        return await this.getSentence();
-    }
-
-     sendSentence(clickedItems) {
-        const obj = {
-            id: this.state.id,
-            moderator: "moderator1@gmail.com",
-            categories: clickedItems
-        };
-
-        return fetch('http://localhost:8080/index.php/sentence',{
-            method: "POST",
-            mode: "cors",
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body:  JSON.stringify(obj)
-        })
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-            if(data.result !== "OK"){
-                console.log("Problem with the server.");
-            }
-        });
+        await this.state.sentence.sendSentence(clickedItems);
+        return await this.state.sentence.getSentence(this.updateStateAfterGetSentence);
     }
 
     render() {
@@ -117,7 +60,8 @@ class App extends Component {
                   <div id="text" className="sentence">{this.state.content}</div>
               </div>
               <AbuseArea handleSubmit={this.handleSubmit} disable={this.state.disable}
-              indicators = {this.state.indicators} toggleDisableButton={this.toggleDisableButton}/>
+              indicators = {this.state.indicators} toggleDisableButton={this.toggleDisableButton}
+              sentence = {this.state.sentence}/>
           </div>
         );
   }
